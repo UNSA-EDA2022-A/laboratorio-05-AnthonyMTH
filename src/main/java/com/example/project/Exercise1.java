221c95cfee91f21f9d66a97a71e28de3dc79cde3
AnthonyTH <90292092+AnthonyMTH@users.noreply.github.com>
2022-06-11 19:57:43 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -19,26 +19,20 @@ public class Exercise1 {
             Character caracter = s.charAt(i); 
             if(caracter == '(' || caracter == ')' || caracter == '[' || 
                 caracter == ']'|| caracter == '{' || caracter == '}') {
-                //Para verificar que solo se ingrese esos caracteres
                 switch (caracter) {
                     case '(': stack.push(caracter); break;
                     case '[': stack.push(caracter); break;
                     case '{': stack.push(caracter); break;
-                    //Si son de apertura se agregan al stack
                     case ')':
                         if (stack.top() == '(') stack.pop(); break;
                     case ']':
                         if (stack.top() == '[') stack.pop(); break;
                     case '}':
                         if (stack.top() == '{') stack.pop(); break;
-                    //Si son se cierre, y el último caracter agregado es 
-                    //el mismo pero de apertura, se elimina
                 }
             }
             else return false;
         }
         return stack.isEmpty(); 
-        //Si aún tiene caracteres quiere decir que no todos los signos 
-        //tuvieron su respectivo cierre. por ende, retorna false
     }
 }
