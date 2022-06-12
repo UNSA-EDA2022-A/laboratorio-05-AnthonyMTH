11b47079536692526bf12e1f87728125ea50ebc2
AnthonyTH <90292092+AnthonyMTH@users.noreply.github.com>
2022-06-11 19:49:33 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -14,9 +14,31 @@ public class Exercise1 {
     }
 
     public boolean esBalanceado(String s) {
-        MyStack<Character> stack = new LinkedListStack<>();
-        // Colocar codigo aqui
-
-        return false;
+        MyStack<Character> stack = new LinkedListStack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            Character caracter = s.charAt(i); 
+            if(caracter == '(' || caracter == ')' || caracter == '[' || 
+                caracter == ']'|| caracter == '{' || caracter == '}') {
+                //Para verificar que solo se ingrese esos caracteres
+                switch (caracter) {
+                    case '(': stack.push(caracter); break;
+                    case '[': stack.push(caracter); break;
+                    case '{': stack.push(caracter); break;
+                    //Si son de apertura se agregan al stack
+                    case ')':
+                        if (stack.top() == '(') stack.pop(); break;
+                    case ']':
+                        if (stack.top() == '[') stack.pop(); break;
+                    case '}':
+                        if (stack.top() == '{') stack.pop(); break;
+                    //Si son se cierre, y el último caracter agregado es 
+                    //el mismo pero de apertura, se elimina
+                }
+            }
+            else return false;
+        }
+        return stack.isEmpty(); 
+        //Si aún tiene caracteres quiere decir que no todos los signos 
+        //tuvieron su respectivo cierre. por ende, retorna false
     }
 }
