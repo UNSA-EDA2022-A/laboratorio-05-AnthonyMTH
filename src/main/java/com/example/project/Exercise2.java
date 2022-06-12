2813e97a1f50f2be05ae9b280458f96a4e1dbd28
AnthonyTH <90292092+AnthonyMTH@users.noreply.github.com>
2022-06-11 19:48:22 -0500

    Update Exercise2.java

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -13,9 +13,39 @@ public class Exercise2 {
     }
 
     public boolean existenDuplicados(String str) {
-        MyStack<Character> stack = new LinkedListStack<>();
-        // Colocar codigo aqui
-
-        return false;
+        MyStack<Character> stack = new LinkedListStack<Character>();
+        int validar = 0;
+        for (int i = 0; i < str.length(); i++) {
+            Character caracter = str.charAt(i);
+            if(caracter == '(' || caracter == ')' || caracter == '+' || 
+                caracter == '-') {
+                //Para verificar que solo se ingrese esos caracteres
+                switch (caracter) {
+                    case '+': if(stack.isEmpty() == false) validar++; break;
+                    case '-': if(stack.isEmpty() == false) validar++; break;
+                    /* Usé como apoyo los signos '+' y '-'. 'validar' es una variable
+                     * que aumentará de valor si hay uno de estos dos signos dentro
+                     * (indicando una operación) y, como condición adicional,
+                     * debe haber un paréntesis ya agregado para verificar correctamente.
+                     * Por ejemplo: '(a+b)+((c+d))' aquí hay 3 '+', sin esta última 
+                     * condición se verificaría como correcto. La condición se aplica
+                     * únicamente para este tipo de casos: '((a+b)-(a-b))'.
+                     */
+                    case '(': stack.push(caracter); break;
+                    case ')':
+                        if(stack.top() == '(' && validar != 0){
+                        //Elimina el último elemento en caso 'validar' sea distinto de 0
+                        //(que haya operación/operaciones dentro)
+                            stack.pop();
+                            validar--;
+                        }; break;
+                }
+            }
+            else continue; //Si no coincide con los caracteres que nos interesan, pasa de ellos
+        }
+        return !stack.isEmpty(); 
+        //Negación porque en caso haya algún caracter al finalizar el bucle, quiere decir que 
+        //hubo paréntesis sobrantes, retornando true (detectando que sí está incorrecto)
+    }
     }
 }
